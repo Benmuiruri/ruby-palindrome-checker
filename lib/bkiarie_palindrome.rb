0270eaaf271488e71f51f89dd09371cc7b42b16c
Benmuiruri <ben.muiruri13@gmail.com>
2022-06-13 07:54:53 +0300

    Write application code to get tests to pass

--- lib/bkiarie_palindrome.rb
+++ lib/bkiarie_palindrome.rb
@@ -7,11 +7,19 @@ class String
     processed_content == processed_content.reverse
   end
 
-  def letters; end
+  def letters
+    the_letters = []
+    for i in 0..self.length - 1
+      if self[i].match(/[a-zA-Z]/)
+        the_letters << self[i]
+      end
+    end
+    the_letters.join
+  end
 
   private
 
   def processed_content
-    downcase
+    self.letters.downcase
   end
 end

c6c5221b90cb5e0af7d53a6ef6e9fe017f3892fd
Benmuiruri <ben.muiruri13@gmail.com>
2022-06-13 07:12:57 +0300

    Initialize repo with bundle gem creation template

---
+++ lib/bkiarie_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "bkiarie_palindrome/version"
+
+module BkiariePalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "bkiarie_palindrome"
+
+require "minitest/autorun"

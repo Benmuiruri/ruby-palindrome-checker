42f5eb496dd7d84aaa2b4f581129e53ec4f7c97d
Benmuiruri <ben.muiruri13@gmail.com>
2022-06-13 08:01:30 +0300

    Refactor letter extracting method

--- lib/bkiarie_palindrome.rb
+++ lib/bkiarie_palindrome.rb
@@ -8,13 +8,7 @@ class String
   end
 
   def letters
-    the_letters = []
-    for i in 0..self.length - 1
-      if self[i].match(/[a-zA-Z]/)
-        the_letters << self[i]
-      end
-    end
-    the_letters.join
+    self.chars.select { |c| c.match(/[a-z]/i) }.join
   end
 
   private
